0e4b5081563b91d79f8d760e1c75c595d4f2f1d6
Munezero Christian <chamsf5@aol.com>
2023-01-11 20:00:28 +0200

    Create dashboard page

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/DashboardPage';
+
+const DashboardPage = () => {
+  return <Wrapper>DashboardPage</Wrapper>;
+};
+
+export default DashboardPage;
