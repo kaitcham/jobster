62baa3bac26094d7c273499924033382a6b4206e
Munezero Christian <chamsf5@aol.com>
2023-01-11 19:22:30 +0200

    Remove nav element form logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,11 +2,7 @@ import React from 'react';
 import logo from '../assets/images/logo.svg';
 
 const Logo = () => {
-  return (
-    <nav className="container">
-      <img src={logo} alt="jobster logo" className="logo" />
-    </nav>
-  );
+  return <img src={logo} alt="jobster logo" className="logo" />;
 };
 
 export default Logo;
