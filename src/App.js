5b9462c6ba1ed5a54ee6b4468f93d7a40002e890
Munezero Christian <chamsf5@aol.com>
2023-01-11 20:02:39 +0200

    Routing and  toasts message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <Fragment>
-      <LandingPage />
-    </Fragment>
+    <BrowserRouter>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
