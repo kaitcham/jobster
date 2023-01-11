36c95f27619b1d694b0d974ab54415c78d47a2e8
Munezero Christian <chamsf5@aol.com>
2023-01-11 19:24:10 +0200

    Create form row input component

---
+++ src/utils/FormRow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FormRow = ({ type, name, value, handleChange }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name}
+      </label>
+
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+};
+
+export default FormRow;
