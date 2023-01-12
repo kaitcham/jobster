04d415e92b6ace578244d79c340d46b78dc2a174
Munezero Christian <chamsf5@aol.com>
2023-01-12 13:12:18 +0200

    Create userSlice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import customBaseUrl from '../../utils/axios';
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+} from '../../utils/localStorage';
+
+const initialState = {
+  isLoading: false,
+  user: getUserFromLocalStorage(),
+};
+
+export const registerUser = createAsyncThunk(
+  'user/registerUser',
+  async (user, thunkApi) => {
+    try {
+      const resp = await customBaseUrl.post('/auth/register', user);
+      return resp.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, thunkApi) => {
+    try {
+      const resp = await customBaseUrl.post('/auth/login', user);
+      return resp.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+      addUserToLocalStorage(action.payload);
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      state.isLoading = false;
+      toast.error(action.payload);
+    });
+    builder.addCase(loginUser.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+      addUserToLocalStorage(action.payload);
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.isLoading = false;
+      toast.error(action.payload);
+    });
+  },
+});
+
+export default userSlice.reducer;
