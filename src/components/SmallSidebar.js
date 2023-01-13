4d2f167c1d53c5c361a2d3a02ef1d107979169fd
Munezero Christian <chamsf5@aol.com>
2023-01-13 14:20:29 +0200

    Modify sidebar component

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { toggleSidebar } from '../features/user/userSlice';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
-const SmallSidebar = () => {
+export const SmallSidebar = () => {
+  const dispatch = useDispatch();
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <div>SmallSidebar</div>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container show-sidebar'
+            : ' sidebar-container'
+        }
+      >
+        <div className="content">
+          <button
+            className="close-btn"
+            onClick={() => dispatch(toggleSidebar())}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={() => dispatch(toggleSidebar())} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
