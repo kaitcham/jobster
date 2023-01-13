9d6bdf41cbbf5f9167506e5190101c76035b053b
Munezero Christian <chamsf5@aol.com>
2023-01-13 14:19:17 +0200

    Modify big sidebar component

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import Logo from '../components/Logo';
+import NavLinks from './NavLinks';
 import Wrapper from '../assets/wrappers/BigSidebar';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
