3fc09fc9e4f5cb89a929a688448f15aa2ab8e538
Munezero Christian <chamsf5@aol.com>
2023-01-13 11:35:14 +0200

    Remove dashboard page and dashboard styles

--- src/pages/DashboardPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Wrapper from '../assets/wrappers/DashboardPage';
-
-const DashboardPage = () => {
-  return <Wrapper>DashboardPage</Wrapper>;
-};
-
-export default DashboardPage;
