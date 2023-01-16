c9512ec70f214197e35650b6a6863af863438f93
Munezero Christian <chamsf5@aol.com>
2023-01-16 13:03:52 +0200

    Add default logout message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
             <button
               type="button"
               className="dropdown-btn"
-              onClick={() => dispatch(logoutUser())}
+              onClick={() => dispatch(logoutUser('You have been logged out'))}
             >
               logout
             </button>
