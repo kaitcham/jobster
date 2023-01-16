335c0ba1ddceb7ab829cf94862dfa7824e986589
Munezero Christian <chamsf5@aol.com>
2023-01-16 10:51:53 +0200

    Add jobs state to the application store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/user/userSlice';
+import jobSlice from './features/job/jobSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    job: jobSlice,
   },
 });
 
