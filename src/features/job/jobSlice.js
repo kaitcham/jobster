effa9192dd8379ddc51ad82a087bc8e2ce5ffd5e
Munezero Christian <chamsf5@aol.com>
2023-01-16 12:12:30 +0200

    Add an action to create a job

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import customBaseUrl from '../../utils/axios';
 
 const initialState = {
   isLoading: false,
@@ -13,6 +15,23 @@ const initialState = {
   editJobId: '',
 };
 
+export const createJob = createAsyncThunk(
+  'job/createJob',
+  async (job, thunkAPI) => {
+    try {
+      const response = await customBaseUrl.post('/jobs', job, {
+        headers: {
+          Authorization: `Bearer ${thunkAPI.getState().user.user.user.token}`,
+        },
+      });
+      thunkAPI.dispatch(clearValues());
+      return response.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const jobSlice = createSlice({
   name: 'job',
   initialState,
@@ -25,6 +44,19 @@ const jobSlice = createSlice({
     clearValues: (state) => {
       return initialState;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createJob.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createJob.fulfilled, (state, action) => {
+      state.isLoading = false;
+      toast.success('Job created successfully');
+    });
+    builder.addCase(createJob.rejected, (state, action) => {
+      state.isLoading = false;
+      toast.error(action.payload);
+    });
   },
 });
 
