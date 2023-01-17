e88eea31647208d2a24a4c42bda549634ba0e2df
Munezero Christian <chamsf5@aol.com>
2023-01-17 10:28:20 +0200

    Add all jobs to the application store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './features/user/userSlice';
 import jobSlice from './features/job/jobSlice';
+import userSlice from './features/user/userSlice';
+import allJobsSlice from './features/allJobs/allJobsSlice';
 
 const store = configureStore({
   reducer: {
-    user: userSlice,
     job: jobSlice,
+    user: userSlice,
+    allJobs: allJobsSlice,
   },
 });
 
