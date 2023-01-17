6ad9899157ee3b22f77d6185045e6d1266143d03
Munezero Christian <chamsf5@aol.com>
2023-01-17 14:07:30 +0200

    Add an action to get all jobs from database

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -3,13 +3,46 @@ import { toast } from 'react-toastify';
 import customBaseUrl from '../../utils/axios';
 
 const initialState = {
-  isLoading: false,
+  isLoading: true,
   jobs: [],
 };
+
+export const getAllJobs = createAsyncThunk(
+  'allJobs/getAllJobs',
+  async (_, thunkApi) => {
+    const url = '/jobs';
+    try {
+      const { data } = await customBaseUrl.get(url, {
+        headers: {
+          authorization: `Bearer ${thunkApi.getState().user.user.user.token}`,
+        },
+      });
+      return data;
+    } catch (error) {
+      thunkApi.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
 
 const allJobsSlice = createSlice({
   name: 'allJobs',
   initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllJobs.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllJobs.fulfilled, (state, action) => {
+        const { jobs } = action.payload;
+        state.isLoading = false;
+        state.jobs = jobs;
+      })
+      .addCase(getAllJobs.rejected, (state, action) => {
+        state.isLoading = false;
+        toast.error(action.payload);
+      });
+  },
 });
 
 export default allJobsSlice.reducer;
