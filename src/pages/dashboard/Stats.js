b5dfdbf1943df9a158a22d7d3334085b50f1c5b5
Munezero Christian <chamsf5@aol.com>
2023-01-25 21:26:39 +0200

    Modify stats page

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import StatsContainer from '../../components/StatsContainer';
+import ChartsContainer from '../../components/ChartsContainer';
+import { showStats } from '../../features/allJobs/allJobsSlice';
 const Stats = () => {
-  return <div>Stats</div>;
+  const { isLoading, monthlyApplications } = useSelector(
+    (state) => state.allJobs
+  );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, [dispatch]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
 };
 
 export default Stats;
