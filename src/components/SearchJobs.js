ac55aab940f3293706fd28cc8d75f0a8462b8d42
Munezero Christian <chamsf5@aol.com>
2023-01-26 15:21:28 +0200

    Create search jobs form

--- src/components/SearchJobs.js
+++ src/components/SearchJobs.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import FormRow from '../utils/FormRow';
+import FormSelect from '../utils/FormSelect';
+import { useSelector, useDispatch } from 'react-redux';
+import Wrapper from '../assets/wrappers/SearchContainer';
 
 const SearchJobs = () => {
-  return <div>SearchJobs</div>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((state) => state.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((state) => state.job);
+  const dispatch = useDispatch();
+  const handleSearch = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>search form</h4>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+          <FormSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleJobInput={handleSearch}
+            arrayData={['all', ...statusOptions]}
+          />
+          <FormSelect
+            labelText="type"
+            name="searchType"
+            value={searchType}
+            handleJobInput={handleSearch}
+            arrayData={['all', ...jobTypeOptions]}
+          />
+          <FormSelect
+            name="sort"
+            value={sort}
+            handleJobInput={handleSearch}
+            arrayData={sortOptions}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchJobs;
